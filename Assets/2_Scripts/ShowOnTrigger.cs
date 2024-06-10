bb0cfb2ecff92eea07d2fe88ff621071a8ef121d
JodiHill <jodi.hilhorst@student.hku.nl>
2024-06-10 16:03:09 +0200

    Interactables done!
    It just needs to be implemented in the main scene

--- Assets/2_Scripts/ShowOnTrigger.cs
+++ Assets/2_Scripts/ShowOnTrigger.cs
@@ -8,8 +8,33 @@ public class ShowOnTrigger : MonoBehaviour
     [Header("The AudioSource to play on trigger")]
     public AudioSource audioSource;
 
+    public float DisableTime { get { return disableTime; } }
+    [SerializeField]
+    private float disableTime;
+
+    private Timer disableTimer;
+
+    private void Start()
+    {
+        disableTimer = new Timer(DisableTime);
+        disableTimer.OnTimer += OnDisableTimer;
+    }
+
+    private void Update()
+    {
+        disableTimer.Tick(Time.deltaTime);
+    }
+
+    private void OnDisable()
+    {
+        disableTimer.OnTimer -= OnDisableTimer;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.layer != LayerMask.NameToLayer("Player")) return;
+        disableTimer.Reset();
+
         if (imageObject != null)
         {
             imageObject.SetActive(true);
@@ -22,8 +47,7 @@ public class ShowOnTrigger : MonoBehaviour
             Debug.Log("Audio started playing");
         }
     }
-
-    private void OnTriggerExit(Collider other)
+    private void OnDisableTimer()
     {
         if (imageObject != null)
         {
